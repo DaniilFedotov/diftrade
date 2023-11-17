476b0723f028e129f53cee369601a47a3f756b9a
Daniil Fedotov <pustovdanil@bk.ru>
2023-11-17 10:41:26 +0500

    PEP8 functions

--- bot_spot_margin/core/functions.py
+++ bot_spot_margin/core/functions.py
@@ -6,10 +6,10 @@ import logging
 def get_logger(name):
     """Создает логгер с заданными параметрами и возвращает его."""
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)  # Назначается минимальный уровень, имея который сообщение идет в лог
+    logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler(stream=sys.stdout)
-    file_handler = logging.FileHandler(f"log_{name}.log", mode='a')  # Обработчик для логирования в файл
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")  # Формат сообщения в лог
+    file_handler = logging.FileHandler(f'log_{name}.log', mode='a')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.addHandler(file_handler)
