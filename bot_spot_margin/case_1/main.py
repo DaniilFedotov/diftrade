9d3ab8446f58acb1b14b3cccde9a01e59d9cd6c4
Daniil Fedotov <pustovdanil@bk.ru>
2023-11-17 10:31:39 +0500

    PEP8 main

--- bot_spot_margin/case_1/main.py
+++ bot_spot_margin/case_1/main.py
@@ -2,18 +2,20 @@ import time
 
 from ..core.functions import get_logger
 from ..core.classes import TraderSpotMargin
-from .settings import TRADER_NAME, TOKEN_NAME, CURRENCY, CLIENT_BINANCE_S, COEF
+from .settings import (TRADER_NAME, TOKEN_NAME,
+                       CURRENCY, CLIENT_BINANCE_S, COEF)
 
 
 def main(bot):
     """Основная логика работы бота."""
     cur_depo = bot.get_balance()
     while True:
-        inlet_factor = bot.check_inlet_condition()  # Фактор входа
-        cur_price = bot.check_price()  # Проверяет текущую цену для проверки уровней
-        level_factor = bot.check_level(cur_price)  # Фактор входа, основанный на уровнях
-        bot.logger.debug(f'inlet_factor: {inlet_factor}, level_factor: {level_factor}')
-        if inlet_factor and level_factor:  # Если оба фактора указывают на вход в сделку
+        inlet_factor = bot.check_inlet_condition()
+        cur_price = bot.check_price()  # Проверяет текущую цену
+        level_factor = bot.check_level(cur_price)
+        bot.logger.debug(f'inlet_factor: {inlet_factor}, '
+                         f'level_factor: {level_factor}')
+        if inlet_factor and level_factor:  # Вход в сделку
             buy_info = bot.buy_coin(cur_depo, cur_price)
             if buy_info == 'canceled':
                 continue
@@ -21,18 +23,20 @@ def main(bot):
             if buy_info['status'] == 'FILLED':
                 sell_info = bot.sell_coin(buy_info)
                 bot.logger.debug(f'sell_info: {sell_info}')
-                profit = float(sell_info['cummulativeQuoteQty']) - float(buy_info['cummulativeQuoteQty'])
+                profit = (float(sell_info['cummulativeQuoteQty']) -
+                          float(buy_info['cummulativeQuoteQty']))
                 message = (f'{bot.name}: Сделка закрыта, заработок: '
                            f'{profit} {bot.currency} '
-                           f'Текущий депозит (ориентировочно): {sell_info["cummulativeQuoteQty"]} {bot.currency}')
-                cur_depo = sell_info["cummulativeQuoteQty"]
+                           f'Текущий депозит (ориентировочно): '
+                           f'{sell_info["cummulativeQuoteQty"]} {bot.currency}')
+                cur_depo = sell_info['cummulativeQuoteQty']
                 bot.logger.info(message)
                 bot.send_message(message)
         timer = bot.get_timer(param='SEARCH')
         time.sleep(timer)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     logger = get_logger(TRADER_NAME)  # Получает логгер
     trader_bot = TraderSpotMargin(
         TRADER_NAME,
